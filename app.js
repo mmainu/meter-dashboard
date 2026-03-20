af3cdae94d778605a7b7d8b23482e9235bdc5cbe
mmainu <79317043+mmainu@users.noreply.github.com>
2026-03-20 17:18:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ==== Config ====
-const AIO_KEY = "aio_OSVI84EarIl2zQ56xq44lVB5bwEs"; // Replace with your Adafruit IO key
+const AIO_KEY = "aio_fXix81ZN13qJi9BVkduww3MRzvaz"; // Replace with your Adafruit IO key
 const FEED = "Mainuddin/feeds/meter";   // Your feed
 
 const clientID = "dashboard-" + Math.floor(Math.random() * 1000);
